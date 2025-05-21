90730a18c9fc45f75dcfbce7d1ecea03c376d194
janek120041 <janekmakulski@gmail.com>
2025-05-21 06:45:00 +0200

    Fix: Add missing SQLAlchemy validates import

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
+from sqlalchemy.orm import validates
 
 @login_manager.user_loader
 def load_user(user_id):
